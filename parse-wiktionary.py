d81ed4b8e941cf01febe62e1363882ede91d6663
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-17 11:50:31 +0700

    Update parse-wiktionary.py

--- parse-wiktionary.py
+++ parse-wiktionary.py
@@ -117,4 +117,4 @@ class IpaParser(sax.handler.ContentHandler):
 if __name__ == "__main__":
     parser = sax.make_parser()
     parser.setContentHandler(IpaParser('Thai')
-    parser.parse(open("enwik.xml"))
+    parser.parse(open("thwik.xml"))
